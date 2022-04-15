1af7eead0a356a2f3b6b7b7c37ff3f21eea90984
Ashish sah <ashishsah1000@gmail.com>
2022-04-15 16:13:33 +0530

    added the table table for showing the data has been added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./styles.css";
 import { Button } from "@mui/material";
+import ItemsTable from "./components/ItemsTable";
 import ResponsiveAppBar from "./components/ResponsiveAppBar";
+
 import Content from "./components/Content";
 
 export default function App() {
   return (
     <div className="App">
-      <ResponsiveAppBar midElement={<Content />} />
+      <ResponsiveAppBar midElement={<ItemsTable />} />
     </div>
   );
 }
---
+++ src/components/ItemsTable.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import { styled } from "@mui/material/styles";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell, { tableCellClasses } from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14
+  }
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  "&:nth-of-type(odd)": {
+    backgroundColor: theme.palette.action.hover
+  },
+  // hide last border
+  "&:last-child td, &:last-child th": {
+    border: 0
+  }
+}));
+
+const ItemsTable = () => {
+  return (
+    <>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 700 }} aria-label="customized table">
+          <TableHead>
+            <TableRow>
+              <StyledTableCell>Id</StyledTableCell>
+              <StyledTableCell align="right">Type</StyledTableCell>
+              <StyledTableCell align="right">Details&nbsp;(g)</StyledTableCell>
+              <StyledTableCell align="right">
+                Created On&nbsp;(g)
+              </StyledTableCell>
+              <StyledTableCell align="right">Price&nbsp;(g)</StyledTableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            <StyledTableRow>
+              {true ? (
+                <StyledTableCell component="th" scope="row">
+                  data is there
+                </StyledTableCell>
+              ) : (
+                <StyledTableCell component="th" scope="row">
+                  seeems quite empty
+                </StyledTableCell>
+              )}
+            </StyledTableRow>
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </>
+  );
+};
+export default ItemsTable;
